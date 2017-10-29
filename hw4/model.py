57f990bdb093eedb5d044df299b119ea60c6ff91
wangyirui <yiruiwang06@gmail.com>
2017-10-29 11:19:15 -0400

    Update model.py

--- hw4/model.py
+++ hw4/model.py
@@ -8,34 +8,40 @@ class RNNLM(nn.Module):
   def __init__(self, vocab_size):
     super(RNNLM, self).__init__()
     # word embedding lookup table
-    self.lookup = nn.Parameter(torch.randn(vocab_size, 32), requires_grad = True)
-    self.weight_x = nn.Parameter(torch.Tensor(32,16), requires_grad = True)
-    self.weight_h = nn.Parameter(torch.Tensor(16,16), requires_grad = True)
-    self.weight_o = nn.Parameter(torch.Tensor(16,vocab_size), requires_grad = True)
+    self.lookup = nn.Parameter(torch.Tensor(vocab_size, 32))
+    self.weight_x = nn.Parameter(torch.Tensor(32, 16))
+    self.weight_h = nn.Parameter(torch.Tensor(16, 16))
+    self.weight_o = nn.Parameter(torch.Tensor(16, vocab_size))
+
+
   def forward(self, input_batch):
     X = self.lookup[input_batch.data,:].data
     sequence_length = input_batch.size()[0]
     batch_length = input_batch.size()[1]
     output = Variable(torch.zeros(sequence_length, batch_length, self.lookup.size()[0]), requires_grad = False)
-    H = nn.Parameter(torch.randn(batch_length, 16), requires_grad = True)
+    H = nn.Parameter(torch.ones(batch_length, 16))
+
 
     for i in range(sequence_length):
 
       input_x = nn.Parameter(X[i,:,:], requires_grad = False)
-      H_tmp1 = input_x.mm(self.weight_x)
-      H_tmp2 = H.mm(self.weight_h)
-      H_sum = H_tmp1 + H_tmp2
+      H_tmp1 = input_x.mm(self.weight_x.float())
+      H_tmp2 = H.mm(self.weight_h.float())
+      H_sum = H_tmp1 + H_tmp2 
       H_cur = torch.tanh(H_sum)
       H = H_cur
-      outlayer_in = H_cur.mm(self.weight_o)
+
+      outlayer_in = H_cur.mm(self.weight_o.float())
       m = nn.Softmax()
       softwax = m(outlayer_in)
+      
       output[i,:,:] = torch.log(softwax)
 
     return output
 
 
 
+ 
 # TODO: Your implementation goes here
 class BiRNNLM(nn.Module):
   def __init__(self, vocab_size):
