3f5355ab1b89f8cdd33abb5b5f3c8b2d0d581760
wangyirui <yiruiwang06@gmail.com>
2017-10-31 13:30:21 -0400

    Update model.py

--- hw4/model.py
+++ hw4/model.py
@@ -9,43 +9,27 @@ class RNNLM(nn.Module):
   def __init__(self, vocab_size):
     super(RNNLM, self).__init__()
     # word embedding lookup table
+    self.vocab_size = vocab_size
     self.lookup   = nn.Parameter(torch.Tensor(vocab_size, 32).uniform_())
     self.weight_x = nn.Parameter(torch.Tensor(32, 16).uniform_(-1.0/math.sqrt(32), 1.0/math.sqrt(32)))
     self.weight_h = nn.Parameter(torch.Tensor(16, 16).uniform_(-1.0/math.sqrt(16), 1.0/math.sqrt(16)))
     self.weight_o = nn.Parameter(torch.Tensor(16, vocab_size).uniform_(-1.0/math.sqrt(16), 1.0/math.sqrt(16)))
 
-    self.bias_x = nn.Parameter(torch.zeros(1))
-    self.bias_h = nn.Parameter(torch.zeros(1))
-    self.bias_o = nn.Parameter(torch.zeros(1))
+    self.bias_x = nn.Parameter(torch.rand(16))
+    self.bias_h = nn.Parameter(torch.rand(16))
+    self.bias_o = nn.Parameter(torch.rand(vocab_size))
 
-    self.H = nn.Parameter(torch.Tensor(1,16).uniform_())
+    self.H = nn.Parameter(torch.Tensor(16).uniform_())
 
 
   def forward(self, input_batch):
-    #print "lookup table:"
-    #print self.lookup[0]
-    #print "weight_x:"
-    #print self.weight_x[0]
-    #print "weight_h:"
-    #print self.weight_h[0]
-    #print "weight_o:"
-    #print self.weight_o[0]
-    print "bias_x:"
-    print self.bias_x
-    print "bias_h:"
-    print self.bias_h
-    print "bias_o:"
-    print self.bias_o
-    #print "H:"
-    #print self.H
-
     sequence_length = input_batch.size()[0]
     batch_length    = input_batch.size()[1]
 
-    X      = Variable(self.lookup[input_batch.data,:].data)
-    output = Variable(torch.zeros(sequence_length, batch_length, self.lookup.size()[0]))
+    X = torch.index_select(self.lookup, 0, input_batch.view(-1)).view(sequence_length, batch_length, 32)
+    H_pre = self.H.expand(batch_length, 16)
 
-    H_pre = self.H
+    outs = []
 
     for i in range(sequence_length):
 
@@ -54,17 +38,19 @@ class RNNLM(nn.Module):
       H_tmp2  = H_pre.mm(self.weight_h) + self.bias_h
       H_sum   = H_tmp1 + H_tmp2
       H_cur   = torch.tanh(H_sum)
-      H_pre  = H_cur[0].view(1,16)
 
-      outlayer_in = H_cur.mm(self.weight_o) + self.bias_o
+      H_pre = H_cur
+
+      outlayer_in = H_cur.mm(self.weight_o)# + self.bias_o
       
       m = nn.Softmax()
 
       softwax = m(outlayer_in)
 
-      output[i,:,:] = torch.log(softwax)
-
-    return output
+      outs.append(torch.log(softwax))
+      #output[i,:,:] = torch.log(softwax)
+    outs = torch.cat(outs)
+    return outs.view(sequence_length, batch_length, self.vocab_size)
 
 
 
@@ -73,7 +59,91 @@ class RNNLM(nn.Module):
 class BiRNNLM(nn.Module):
   def __init__(self, vocab_size):
     super(BiRNNLM, self).__init__()
-    pass
+    # word embedding lookup table
+    self.vocab_size = vocab_size
+    #self.lookup   = nn.Parameter(torch.Tensor(vocab_size, 32).uniform_())
+    self.lookup   = nn.Parameter(torch.Tensor(vocab_size, 32).uniform_(-1.0/math.sqrt(32), 1.0/math.sqrt(32)))
+
+    self.weight_xf = nn.Parameter(torch.Tensor(32, 8).uniform_(-1.0/math.sqrt(8), 1.0/math.sqrt(8)))
+    self.weight_hf = nn.Parameter(torch.Tensor(8, 8).uniform_(-1.0/math.sqrt(8), 1.0/math.sqrt(8)))
+
+    self.weight_xb = nn.Parameter(torch.Tensor(32, 8).uniform_(-1.0/math.sqrt(8), 1.0/math.sqrt(8)))
+    self.weight_hb = nn.Parameter(torch.Tensor(8, 8).uniform_(-1.0/math.sqrt(8), 1.0/math.sqrt(8)))
+    self.weight_o = nn.Parameter(torch.Tensor(16, vocab_size).uniform_(-1.0/math.sqrt(vocab_size), 1.0/math.sqrt(vocab_size)))
+
+    self.Hf = nn.Parameter(torch.Tensor(8).uniform_(-1.0/math.sqrt(8), 1.0/math.sqrt(8)))
+    self.Hb = nn.Parameter(torch.Tensor(8).uniform_(-1.0/math.sqrt(8), 1.0/math.sqrt(8)))
+
+    #self.Hf = nn.Parameter(torch.ones(8))
+    #self.Hb = nn.Parameter(torch.ones(8))
+
+
+    self.bias_x = nn.Parameter(torch.Tensor(8).uniform_(-1.0/math.sqrt(8), 1.0/math.sqrt(8)))
+    self.bias_hf = nn.Parameter(torch.Tensor(8).uniform_(-1.0/math.sqrt(8), 1.0/math.sqrt(8)))
+    self.bias_hb = nn.Parameter(torch.Tensor(8).uniform_(-1.0/math.sqrt(8), 1.0/math.sqrt(8)))
+    self.bias_o = nn.Parameter(torch.Tensor(vocab_size).uniform_(-1.0/math.sqrt(vocab_size), 1.0/math.sqrt(vocab_size)))
 
   def forward(self, input_batch):
-    pass
+    sequence_length = input_batch.size()[0]
+    batch_length    = input_batch.size()[1]
+
+    X = torch.index_select(self.lookup, 0, input_batch.view(-1)).view(sequence_length, batch_length, 32)
+    Hf_pre = self.Hf.expand(batch_length, 8)
+    Hb_pre = self.Hb.expand(batch_length, 8)
+
+    Hf_table = [] 
+    Hb_table = [] 
+
+    # forward
+    # save the inital H state
+    Hf_table.append(Hf_pre)
+    for i in range(sequence_length):
+
+      input_x = X[i,:,:]
+
+      H_tmp1  = input_x.mm(self.weight_xf) + self.bias_x
+      H_tmp2  = Hf_pre.mm(self.weight_hf) + self.bias_hf
+      H_sum   = H_tmp1 + H_tmp2
+      H_cur   = torch.tanh(H_sum)
+      Hf_pre = H_cur
+      Hf_table.append(H_cur)
+
+    Hf_table = Hf_table[:-1]
+    Hf_table = torch.cat(Hf_table)
+    Hf_table = Hf_table.view(sequence_length, batch_length, -1)
+
+    # backward
+    # save the initial H state
+    Hb_table.append(Hb_pre)
+    for i in range(sequence_length-1,-1, -1):
+
+      input_x = X[i,:,:]
+      
+      H_tmp1  = input_x.mm(self.weight_xb) + self.bias_x
+      H_tmp2  = Hb_pre.mm(self.weight_hb) + self.bias_hb
+      H_sum   = H_tmp1 + H_tmp2
+      H_cur   = torch.tanh(H_sum)
+      Hb_pre = H_cur
+      Hb_table.append(H_cur)
+
+    # reverse backward hidden layer result to fit forward
+    Hb_table = Hb_table[:-1]
+    Hb_table.reverse()
+    Hb_table = torch.cat(Hb_table)
+    Hb_table = Hb_table.view(sequence_length, batch_length, -1)
+
+    outs = []
+
+    for i in range(sequence_length):
+      H_i = torch.cat((Hf_table[i],Hb_table[i]),1)
+      outlayer_in = H_i.mm(self.weight_o) + self.bias_o
+      
+      m = nn.Softmax()
+
+      softwax = m(outlayer_in)
+
+      outs.append(torch.log(softwax))
+    outs = torch.cat(outs)
+    return outs.view(sequence_length, batch_length, self.vocab_size)
+
+
