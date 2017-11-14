c5df30d1293faa3e9416713249c0c0977864e6c3
wangyirui <yiruiwang06@gmail.com>
2017-11-14 13:29:45 -0500

    change model file

--- hw5/model.py
+++ hw5/model.py
@@ -37,15 +37,15 @@ class NMT(nn.Module):
     self.lstm_en.bias_hh_l0_reverse.data = (model_param["encoder.rnn.bias_hh_l0_reverse"])
 
     # decoder
-    self.lstm_de = nn.LSTM(self.src_word_emb_size + self.context_vector_size, self.decoder_hidden_size)
+    self.lstm_de = nn.LSTMCell(self.src_word_emb_size + self.context_vector_size, self.decoder_hidden_size)
     # lstm_de forward
-    self.lstm_de.weight_ih_l0.data = (model_param["decoder.rnn.layers.0.weight_ih"])
-    self.lstm_de.weight_hh_l0.data = (model_param["decoder.rnn.layers.0.weight_hh"])
-    self.lstm_de.bias_ih_l0.data = (model_param["decoder.rnn.layers.0.bias_ih"])
-    self.lstm_de.bias_hh_l0.data = (model_param["decoder.rnn.layers.0.bias_hh"])
+    self.lstm_de.weight_ih.data = (model_param["decoder.rnn.layers.0.weight_ih"])
+    self.lstm_de.weight_hh.data = (model_param["decoder.rnn.layers.0.weight_hh"])
+    self.lstm_de.bias_ih.data = (model_param["decoder.rnn.layers.0.bias_ih"])
+    self.lstm_de.bias_hh.data = (model_param["decoder.rnn.layers.0.bias_hh"])
 
     # generator
-    self.generator = nn.Linear(vocab_size, self.decoder_hidden_size)
+    self.generator = nn.Linear(23262, self.decoder_hidden_size)
     self.generator.weight.data = (model_param["0.weight"])
     self.generator.bias.data = (model_param["0.bias"])
 
@@ -57,8 +57,6 @@ class NMT(nn.Module):
     # self.weight_o.weight.data = (model_param["decoder.attn.linear_out.weight"])
 
     self.weight_o = nn.Parameter(model_param["decoder.attn.linear_out.weight"])
-
-    self.ht = Variable(torch.Tensor(self.decoder_hidden_size).uniform_())
 
     self.tanh = nn.Tanh()
     self.softmax = nn.Softmax()
@@ -80,29 +78,24 @@ class NMT(nn.Module):
     word_embed_en = self.embeddings_en(train_src_batch)
     output_hs, (h,c) = self.lstm_en(word_embed_en) # sequence_length x batch_length x 1024
 
-    h = h.permute(1,2,0).contiguous().view(batch_length, 1024)
-    c = c.permute(1,2,0).contiguous().view(batch_length, 1024)
-
-    ht_pre = self.ht.expand(batch_length, self.decoder_hidden_size)
+    h = h.contiguous().view(batch_length, 1024)
+    c = c.contiguous().view(batch_length, 1024)
    
-    vocab_distrubition = self.logsoftmax(self.generator(ht_pre))
+    vocab_distrubition = self.logsoftmax(self.generator(h))
 
     output = Variable(torch.Tensor(trg_sequence_lentgh, batch_length, 23262))
     output[0] = Variable(torch.Tensor(batch_length,23262).fill_(0))
-
-    
-    s_t = Variable(torch.zeros(self.decoder_hidden_size)).expand(batch_length,self.decoder_hidden_size)
 
     # attention, output is context vector ct 
 
     for i in range(1,trg_sequence_lentgh):
 
         a = Variable(torch.Tensor(sequence_length, batch_length))
-        s_t = Variable(torch.Tensor(batch_length, 1024).fill_(1./1024))
+        s_t = Variable(torch.Tensor(batch_length, 1024))
         #print i
 
         for j in range(sequence_length):        
-            score_pre =self.weight_i(ht_pre)
+            score_pre =self.weight_i(h)
             #print score_pre
 
             score = output_hs[j].mm(torch.t(score_pre))   # 48 x 48
@@ -118,13 +111,12 @@ class NMT(nn.Module):
         s_t = torch.sum(a * output_hs, dim=0)   # 48 x 1024
         #print s_t
 
-        ct_input = torch.cat((s_t,ht_pre),1)  # 48 x 2048
+        ct_input = torch.cat((s_t,h),1)  # 48 x 2048
         #print ct_input  
 
         c_t = self.tanh(self.weight_o.mm(torch.t(ct_input))) # 1024 x 48
         #print c_t
-    
-        
+           
         # decoder
         if train_trg_batch is None:
             (_, argmax) = torch.max(vocab_distrubition,1)
@@ -134,19 +126,14 @@ class NMT(nn.Module):
 
        
         decoder_input = torch.cat((torch.t(c_t), word_embed_de),1) # 48 x1324
-        decoder_input = decoder_input.view(1, decoder_input.size()[0], decoder_input.size()[1])
         #print decoder_input.size()
-
-        ht_pre = ht_pre.contiguous().view(1, ht_pre.size()[0], ht_pre.size()[1])
-        c = c.contiguous().view(1, c.size()[0], c.size()[1])
         
-        _, (ht_pre,c) = self.lstm_de(decoder_input, (ht_pre,c))
-        ht_pre = ht_pre[0] # 48 x 1024
-        c = c[0] # 48 x 1024
-        #print c.size()
+        h, c = self.lstm_de(decoder_input, (h,c))
+
+        #print h.size()
 
         # generator
-        vocab_distrubition = self.logsoftmax(self.generator(ht_pre))
+        vocab_distrubition = self.logsoftmax(self.generator(h))
         output[i] = vocab_distrubition
 
     return output
