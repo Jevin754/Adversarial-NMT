05ef4fa9622a68bf91f93120afbcd2e83c3505f7
wangyirui <yiruiwang06@gmail.com>
2017-11-17 23:06:44 -0500

    fix the computing path

--- hw5/model.py
+++ hw5/model.py
@@ -6,17 +6,16 @@ import numpy as np
 import math
 
 class NMT(nn.Module):
-    def __init__(self, trg_vocab_size):
+    def __init__(self, trg_vocab_size, use_cuda = True):
         super(NMT, self).__init__()
 
-        self.train = True
+        self.use_cuda = use_cuda
 
         self.src_word_emb_size = 300
         self.encoder_hidden_size = 512
         self.decoder_hidden_size = 1024
         self.context_vector_size = 1024
         self.trg_vocab_size = trg_vocab_size
-
         model_param = torch.load(open("data/model.param", 'rb'))
 
 
@@ -41,6 +40,7 @@ class NMT(nn.Module):
 
         # decoder
         self.lstm_de = nn.LSTMCell(self.src_word_emb_size + self.context_vector_size, self.decoder_hidden_size)
+
         # lstm_de forward
         self.lstm_de.weight_ih.data = (model_param["decoder.rnn.layers.0.weight_ih"])
         self.lstm_de.weight_hh.data = (model_param["decoder.rnn.layers.0.weight_hh"])
@@ -59,14 +59,19 @@ class NMT(nn.Module):
         self.weight_o = nn.Linear(2048,1024,bias = False)
         self.weight_o.weight.data = (model_param["decoder.attn.linear_out.weight"])
 
-        #self.weight_o = nn.Parameter(model_param["decoder.attn.linear_out.weight"])
+        # other initialzation
+        if use_cuda:
+            self.init_embedding_de = nn.Parameter(torch.zeros(1, self.src_word_emb_size)).cuda()
+            self.init_st = nn.Parameter(torch.zeros(1, self.context_vector_size)).cuda()
+        else:
+            self.init_embedding_de = nn.Parameter(torch.zeros(1, self.src_word_emb_size))
+            self.init_st = nn.Parameter(torch.zeros(1, self.context_vector_size))
 
         self.tanh = nn.Tanh()
-        # self.softmax = nn.Softmax()
-        # self.logsoftmax = nn.functional.log_softmax()
 
 
-    def forward(self, train_src_batch, train_trg_batch = None, is_train = True, use_cuda = True):
+
+    def forward(self, train_src_batch, train_trg_batch = None, is_train = True):
         trg_seq_length = train_trg_batch.size()[0]
         batch_length   = train_src_batch.size()[1]
 
@@ -74,24 +79,10 @@ class NMT(nn.Module):
         src_embed = self.embeddings_en(train_src_batch)
 
         # Encoding
-        # encoding_o, (encoding_h,encoding_c) = self.lstm_en(src_embed) # (seq_len, batch, hidden_size * num_directions)
         encoding_o, (e_h, e_c) = self.lstm_en(src_embed) # (seq_len, batch, hidden_size * num_directions)
 
-        # if use_cuda:
-        #     output = Variable(torch.Tensor(trg_seq_length, batch_length, 23262)).cuda()
-        # else:
-        #     output = Variable(torch.Tensor(trg_seq_length, batch_length, 23262))
 
         output = []
-
-        if use_cuda:
-            output.append(Variable(torch.FloatTensor(batch_length, 23262).fill_(0).cuda()).unsqueeze(0))
-        else:
-            output.append(Variable(torch.FloatTensor(batch_length, 23262).fill_(0)).unsqueeze(0))
-
-        # # Initialize start vocab_distribution
-        # stdv = 1.0 / math.sqrt(23262)
-        # vocab_distrubition = torch.Tensor(batch_length, 23262).uniform_(-stdv, stdv)
 
         # Pre-compute attention score matrix left part
         mat_left_mul = encoding_o.matmul(self.weight_i.weight)  # (seq_len, batch_size, 1024)
@@ -100,41 +91,40 @@ class NMT(nn.Module):
         if train_trg_batch is not None:
             d_embed = self.embeddings_de(train_trg_batch)
 
-        # Initialize argmax
-        argmax = Variable(torch.LongTensor(batch_length).fill_(2))
-        if use_cuda:
-            argmax = argmax.cuda()
-
-        for i in range(1, trg_seq_length):
+        for i in range(trg_seq_length):
             # Initialize the pesudo decoding hidden state, cell state
-            if i == 1:
+            if i == 0:
                 d_h = torch.cat(e_h, 1)
                 d_c = torch.cat(e_c, 1)
 
-            if use_cuda:
+            if self.use_cuda:
                 d_h = Variable(d_h.data).cuda()
                 d_c = Variable(d_c.data).cuda()
+
             else:
                 d_h = Variable(d_h.data)
                 d_c = Variable(d_c.data)
 
-            # d_h = d_h.detach()
-            # d_c = d_c.detach()
+            # Decoding
+            if is_train:
+                if i == 0:
+                    decoder_input = torch.cat((self.init_st.expand(batch_length, self.context_vector_size),
+                                               self.init_embedding_de.expand(batch_length, self.src_word_emb_size)), 1)
+                else:
+                    decoder_input = torch.cat((s_t, d_embed[i - 1, :, :]), 1)
+            else:
+                if i == 0:
+                    decoder_input = torch.cat((self.init_st.expand(batch_length, self.context_vector_size),
+                                               self.init_embedding_de.expand(batch_length, self.src_word_emb_size)), 1)  # 48 x1324
+                else:
+                    word_embed_de = self.embeddings_de(argmax)
+                    decoder_input = torch.cat((s_t, word_embed_de), 1)  # 48 x1324
+            d_h, d_c = self.lstm_de(decoder_input, (d_h, d_c))
 
-            # Compute attention
-            # scores = []
-
-            # Compute score for each word
-            
-            # for idx, hidden_state in enumerate(encoding_o):
-            #     tmp = torch.sum(mat_left_mul[idx] * decoding_h, 1).view(-1, 1)
-            #     scores.append(tmp)
-
-            # scores = torch.cat([torch.sum(mat_left_mul[idx] * d_h, 1).view(-1, 1) for idx, _ in enumerate(encoding_o)], 1) # (batch_size, sequence_len)
             scores = torch.cat([torch.sum(mat_left_mul[idx] * d_h, 1).view(-1, 1) for idx in range(encoding_o.size(0))],1)  # (batch_size, sequence_len)
 
             association = torch.exp(scores)
-            association = (association / torch.sum(association, 1).unsqueeze(1) \
+            association = (association / torch.sum(association, 1).unsqueeze(1)
                             .expand(scores.size(0), scores.size(1))).t() # (sequence_len, batch)
 
             # Compute context vector
@@ -144,20 +134,7 @@ class NMT(nn.Module):
 
             c_t = self.tanh(self.weight_o(torch.cat([s_t, d_h], 1)))
 
-            # Decoding
-            if is_train:
-                decoder_input = torch.cat(((Variable(c_t.data)), d_embed[i-1,:,:]), 1)
-            else:
-                # (_, argmax) = torch.max(vocab_distrubition, 1)
-                word_embed_de = self.embeddings_de(argmax)
-                decoder_input = torch.cat((c_t, word_embed_de), 1)  # 48 x1324
-
-            d_h, d_c = self.lstm_de(decoder_input, (d_h, d_c))
-
             # Generator
-            # vocab_distrubition = self.logsoftmax((self.generator(d_h)).clamp(min=1e-8))
-            # vocab_distrubition = self.logsoftmax((self.generator(d_h)))
-            # vocab_distrubition = nn.functional.log_softmax(self.generator(d_h))
             vocab_distrubition = nn.functional.log_softmax(self.generator(c_t)).unsqueeze(0)
 
             (_, argmax) = torch.max(vocab_distrubition[0], 1)
