5a4229655a70ad21c1cd2e1ac417d76eceeb54bb
Xuan Zhang <xuanzhang@jhu.edu>
2017-12-07 09:26:03 -0500

    add discriminator

---
+++ discriminator.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F 
+from torch.autograd import Variable
+import numpy as np 
+
+class Discriminator(nn.Module):
+    def __init__(self, src_vocab_size, trg_vocab_size, word_emb_size, 
+        src_vocab, trg_vocab, use_cuda=False):
+        super(Discriminator, self).__init__()
+
+        self.src_vocab_size = src_vocab_size
+        self.trg_vocab_size = trg_vocab_size
+        self.word_emb_size = word_emb_size
+        self.src_vocab = src_vocab
+        self.trg_vocab = trg_vocab
+        self.use_cuda = use_cuda
+        
+        self.embedding1 = nn.Embedding(src_vocab_size, word_emb_size)
+        self.embedding2 = nn.Embedding(trg_vocab_size, word_emb_size)
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels = word_emb_size*2,
+                      out_channels = 64,
+                      kernel_size = 3,
+                      sride = 1,
+                      padding = 1),
+            nn.BatchNorm2d(64),
+            nn.Relu(),
+            nn.MaxPool2d(kernel_size = 2,
+                         stride = 2))
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels = 64,
+                      out_channels = 20,
+                      kernel_size = 3,
+                      stride = 1,
+                      padding = 1),
+            nn.BatchNorm2d(20),
+            nn.Relu(),
+            nn.MaxPool2d(kernel_size = 2,
+                         stride = 2))
+
+        self.mlp = nn.Linear(1280, 20)
+        self.ll = nn.Linear(20,1)
+        self.softmax = nn.Softmax() 
+
+    def forward(self, src_batch, trg_batch):
+        # src_batch : (src_seq_len, batch_size)
+        src_embedded = self.embedding1(src_batch) #(s,e,b)
+        trg_embedded = self.embedding2(trg_batch)
+
+        src_padded = np.zeros((32,self.word_emb_size, src_batch.size(-1)))
+        trg_padded = np.zeros((32,self.word_emb_size, src_batch.size(-1)))
+        src_padded[:src_embedded.size(0), :src_embedded.size(1), :src_embedded.size(2)] = src_embedded.data
+        trg_padded[:trg_embedded.size(0), :trg_embedded.size(1), :trg_embedded.size(2)] = trg_embedded.data
+
+        src_padded = np.transpose(np.expand_dims(src_padded,2),(3,1,2,0)) #(b,c,h,w)
+        src_padded = np.concatenate([src_padded]*3, axis=2)
+        trg_padded = np.transpose(np.expand_dims(trg_padded,2),(3,1,0,2))
+        trg_padded = np.concatenate([trg_padded]*3, axis=2)
+
+        input = Variable(torch.from_array(np.concatenate([src_padded,trg_padded],axis=2)))
+
+        if self.use_cuda:
+            input = input.cuda()
+
+        out = self.conv1(input)
+        out = self.conv2(out)
+        out = torch.view(out.size(0), 1280)
+        out = F.relu(self.mlp(out))
+        out = self.ll(out)
+        out = self.softmax(out)
+
+        return out
+
+
---
+++ train_gan.py
@@ -0,0 +1,194 @@
+import utils.tensor
+import utils.rand
+
+import argparse
+import dill
+import logging
+
+import torch
+from torch import cuda
+from torch.autograd import Variable
+from model import NMT, EncoderRNN, Attn, LuongAttnDecoderRNN
+from discriminator import Discriminator
+
+ random 
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG)
+
+parser = argparse.ArgumentParser(description="Starter code for JHU CS468 Machine Translation Final Project.")
+parser.add_argument("--data_file", required=True,
+                    help="File prefix for training set.")
+parser.add_argument("--src_lang", default="de",
+                    help="Source Language. (default = de)")
+parser.add_argument("--trg_lang", default="en",
+                    help="Target Language. (default = en)")
+parser.add_argument("--model_file", required=True,
+                    help="Location to dump the models.")
+parser.add_argument("--batch_size", default=1, type=int,
+                    help="Batch size for training. (default=1)")
+parser.add_argument("--epochs", default=20, type=int,
+                    help="Epochs through the data. (default=20)")
+parser.add_argument("--optimizer", default="SGD", choices=["SGD", "Adadelta", "Adam"],
+                    help="Optimizer of choice for training. (default=SGD)")
+parser.add_argument("--learning_rate", "-lr", default=0.1, type=float,
+                    help="Learning rate of the optimization. (default=0.1)")
+parser.add_argument("--momentum", default=0.9, type=float,
+                    help="Momentum when performing SGD. (default=0.9)")
+parser.add_argument("--estop", default=1e-2, type=float,
+                    help="Early stopping criteria on the development set. (default=1e-2)")
+parser.add_argument("--gpuid", default=[], nargs='+', type=int,
+                    help="ID of gpu device to use. Empty implies cpu usage.")
+# feel free to add more arguments as you need
+
+def main(options):
+
+  use_cuda = (len(options.gpuid) >= 1)
+  if options.gpuid:
+    cuda.set_device(options.gpuid[0])
+
+  src_train, src_dev, src_test, src_vocab = torch.load(open(options.data_file + "." + options.src_lang, 'rb'))
+  trg_train, trg_dev, trg_test, trg_vocab = torch.load(open(options.data_file + "." + options.trg_lang, 'rb'))
+
+  batched_train_src, batched_train_src_mask, sort_index = utils.tensor.advanced_batchize(src_train, options.batch_size, src_vocab.stoi["<blank>"])
+  batched_train_trg, batched_train_trg_mask = utils.tensor.advanced_batchize_no_sort(trg_train, options.batch_size, trg_vocab.stoi["<blank>"], sort_index)
+  batched_dev_src, batched_dev_src_mask, sort_index = utils.tensor.advanced_batchize(src_dev, options.batch_size, src_vocab.stoi["<blank>"])
+  batched_dev_trg, batched_dev_trg_mask = utils.tensor.advanced_batchize_no_sort(trg_dev, options.batch_size, trg_vocab.stoi["<blank>"], sort_index)
+
+  trg_vocab_size = len(trg_vocab)
+  src_vocab_size = len(src_vocab)
+  word_emb_size = 300
+  hidden_size = 1024
+
+  nmt = NMT(src_vocab_size, trg_vocab_size, word_emb_size, hidden_size,
+            src_vocab, trg_vocab, attn_model = "general", use_cuda = True)
+  discriminator = Discriminator(src_vocab_size, trg_vocab_size, word_emb_size, 
+            src_vocab, trg_vocab, use_cuda = True)
+
+  if use_cuda > 0:
+    nmt.cuda()
+  else:
+    nmt.cpu()
+
+  criterion_g = torch.nn.NLLLoss()
+  criterion = torch.nn.CrossEntropyLoss()
+
+  # Configure optimization
+  optimizer_g = eval("torch.optim." + options.optimizer)(nmt.parameters(), options.learning_rate)
+  optimizer_d = eval("torch.optim." + options.optimizer)(discriminator.parameters(), options.learning_rate)
+  
+  # main training loop
+  last_dev_avg_loss = float("inf")
+  for epoch_i in range(options.epochs):
+    logging.info("At {0}-th epoch.".format(epoch_i))
+    # srange generates a lazy sequence of shuffled range
+    for i, batch_i in enumerate(utils.rand.srange(len(batched_train_src))):
+      train_src_batch = Variable(batched_train_src[batch_i])  # of size (src_seq_len, batch_size)
+      train_trg_batch = Variable(batched_train_trg[batch_i])  # of size (src_seq_len, batch_size)
+      train_src_mask = Variable(batched_train_src_mask[batch_i])
+      train_trg_mask = Variable(batched_train_trg_mask[batch_i])
+      if use_cuda:
+        train_src_batch = train_src_batch.cuda()
+        train_trg_batch = train_trg_batch.cuda()
+        train_src_mask = train_src_mask.cuda()
+        train_trg_mask = train_trg_mask.cuda()
+
+      sys_out_batch = nmt(train_src_batch, train_trg_batch, True)
+      fake_dis_label_out = discriminator(train_src_batch, train_trg_batch, True)
+      real_dis_label_out = discriminator(train_src_batch, sys_out_batch, True)
+
+      sys_out_label = Variable(torch.zeros(options.batch_size))
+      train_trg_label = Variable(torch.ones(options.batch_size))
+      if use_cuda > 0:
+        sys_out_batch = sys_out_batch.cuda()
+        train_trg_batch = train_trg_batch.cuda()
+      else:
+        sys_out_batch = sys_out_batch.cpu()
+        train_trg_batch = train_trg_batch.cpu()
+
+      if random.random()>0.5:
+        train_trg_mask = train_trg_mask.view(-1)
+        train_trg_batch = train_trg_batch.view(-1)
+        train_trg_batch = train_trg_batch.masked_select(train_trg_mask)
+        train_trg_mask = train_trg_mask.unsqueeze(1).expand(len(train_trg_mask), trg_vocab_size)
+        sys_out_batch = sys_out_batch.view(-1, trg_vocab_size)
+        sys_out_batch = sys_out_batch.masked_select(train_trg_mask).view(-1, trg_vocab_size)
+        loss_g = criterion_g(sys_out_batch, train_trg_batch)
+      else:
+        loss_g = criterion(1, fake_dis_label_out)
+      loss_d = criterion(1, real_dis_label_out) + criterion(0, fake_dis_label_out)
+
+      logging.debug("G loss at batch {0}: {1}".format(i, loss_g.data[0]))
+      logging.debug("D loss at batch {0}: {1}".format(i, loss_d.data[0]))
+      train_loss_g += loss_g
+      train_loss_d += loss_d
+    train_avg_loss_g = train_loss_g / len(train_src_batch)
+    train_avg_loss_d = train_loss_d / len(train_src_batch)
+    logging.info("G TRAIN Average loss value per instance is {0} at the end of epoch {1}".format(train_avg_loss_g.data[0], epoch_i))
+    logging.info("D TRAIN Average loss value per instance is {0} at the end of epoch {1}".format(train_avg_loss_d.data[0], epoch_i))
+      
+      optimizer_g.zero_grad()
+      optimizer_d.zero_grad()
+      loss_g.backward()
+      loss_d.backward()
+      # # gradient clipping
+      torch.nn.utils.clip_grad_norm(nmt.parameters(), 5.0)
+      optimizer_g.step()
+      optimizer_d.step()
+
+    # validation -- this is a crude esitmation because there might be some paddings at the end
+    dev_loss = 0.0
+    for batch_i in range(len(batched_dev_src)):
+      dev_src_batch = Variable(batched_dev_src[batch_i], volatile=True)
+      dev_trg_batch = Variable(batched_dev_trg[batch_i], volatile=True)
+      dev_src_mask = Variable(batched_dev_src_mask[batch_i], volatile=True)
+      dev_trg_mask = Variable(batched_dev_trg_mask[batch_i], volatile=True)
+      if use_cuda:
+        dev_src_batch = dev_src_batch.cuda()
+        dev_trg_batch = dev_trg_batch.cuda()
+        dev_src_mask = dev_src_mask.cuda()
+        dev_trg_mask = dev_trg_mask.cuda()
+
+      sys_out_batch = nmt(dev_src_batch, dev_trg_batch, False)
+      fake_dis_label_out = discriminator(dev_src_batch, dev_trg_batch, True)
+      real_dis_label_out = discriminator(dev_src_batch, sys_out_batch, True)
+
+      sys_out_label = Variable(torch.zeros(options.batch_size))
+      dev_trg_label = Variable(torch.ones(options.batch_size))
+      if use_cuda > 0:
+        sys_out_batch = sys_out_batch.cuda()
+        dev_trg_batch = dev_trg_batch.cuda()
+      else:
+        sys_out_batch = sys_out_batch.cpu()
+        dev_trg_batch = dev_trg_batch.cpu()
+
+      dev_trg_mask = dev_trg_mask.view(-1)
+      dev_trg_batch = dev_trg_batch.view(-1)
+      dev_trg_batch = dev_trg_batch.masked_select(dev_trg_mask)
+      dev_trg_mask = dev_trg_mask.unsqueeze(1).expand(len(dev_trg_mask), trg_vocab_size)
+      sys_out_batch = sys_out_batch.view(-1, trg_vocab_size)
+      sys_out_batch = sys_out_batch.masked_select(dev_trg_mask).view(-1, trg_vocab_size)
+      loss_g = criterion(1, fake_dis_label_out)
+      loss_d = criterion(1, real_dis_label_out) + criterion(0, fake_dis_label_out)
+      logging.debug("G dev loss at batch {0}: {1}".format(batch_i, loss_g.data[0]))
+      logging.debug("D dev loss at batch {0}: {1}".format(batch_i, loss_d.data[0]))
+      dev_loss_g += loss_g
+      dev_loss_d += loss_d
+    dev_avg_loss_g = dev_loss_g / len(batched_dev_src)
+    dev_avg_loss_d = dev_loss_d / len(batched_dev_src)
+    logging.info("G DEV Average loss value per instance is {0} at the end of epoch {1}".format(dev_avg_loss_g.data[0], epoch_i))
+    logging.info("D DEV Average loss value per instance is {0} at the end of epoch {1}".format(dev_avg_loss_d.data[0], epoch_i))
+    # if (last_dev_avg_loss - dev_avg_loss).data[0] < options.estop:
+    #   logging.info("Early stopping triggered with threshold {0} (previous dev loss: {1}, current: {2})".format(epoch_i, last_dev_avg_loss.data[0], dev_avg_loss.data[0]))
+    #   break
+    torch.save(nmt, open(options.model_file + ".nll_{0:.2f}.epoch_{1}".format(dev_avg_loss_d.data[0], epoch_i), 'wb'), pickle_module=dill)
+    last_dev_avg_loss = dev_avg_loss
+
+
+if __name__ == "__main__":
+  ret = parser.parse_known_args()
+  options = ret[0]
+  if ret[1]:
+    logging.warning("unknown arguments: {0}".format(parser.parse_known_args()[1]))
+  main(options)
