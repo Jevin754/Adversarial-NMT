140ce78eedc6a2c6643f992ce154b893c9d7024f
Xuan Zhang <xuanzhang@jhu.edu>
2017-12-07 19:06:18 -0500

    torch.nn

--- NMT/train_gan.py
+++ NMT/train_gan.py
@@ -103,8 +103,8 @@ def main(options):
             src_vocab, trg_vocab, use_cuda = True)
 
   if use_cuda > 0:
-    nmt = torch.nn.DataParallel(nmt).cuda()
-    discriminator = torch.nn.DataParallel(discriminator).cuda()
+    nmt = torch.nn.DataParallel(nmt,device_ids=options.gpuid).cuda()
+    discriminator = torch.nn.DataParallel(discriminator,device_ids=options.gpuid).cuda()
   else:
     nmt.cpu()
     discriminator.cpu()
